5313694475559642b5687f68cde6e9d3691dffce
nouran-essam <nouranessam885@gmail.com>
2023-10-10 11:14:50 -0700

    s

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,24 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * _memset - copy char
- * @s: string
- * @b: input
- * @n: bytes
- * Return: string
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: n elements
+ * @size: bytes
+ * Return: pointer
  */
 
-#include <stdlib.h>
-#include <string.h>
-
 void *_calloc(unsigned int nmemb, unsigned int size) {
-    if (nmemb == 0 || size == 0) {
-        return NULL;
-    }
-
     void *ptr = malloc(nmemb * size);
     if (ptr == NULL) {
         return NULL;
     }
-
- memset(ptr, 0, nmemb * size);
+    memset(ptr, 0, nmemb * size);
     return ptr;
+}
+
+int main() {
+    int *arr;
+    int i, n = 5;
+
+    arr = (int *)_calloc(n, sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    free(arr);
+    return 0;
 }
